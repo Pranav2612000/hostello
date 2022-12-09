365e28e4aa8774b34dc06696ec8d9a9dad962691
rishim1402 <rishim1402@gmail.com>
2022-12-10 00:39:44 +0530

    fix: add _id in hostel entity

--- backend/hostello/entity/hostel.go
+++ backend/hostello/entity/hostel.go
@@ -1,5 +1,7 @@
 package entity
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
 	FistName string `json:"firstName"`
 	LastName string `json:"lastName"`
@@ -8,24 +10,25 @@ type User struct {
 }
 
 type Hostel struct {
-	HostelName       string `json:"hostelName"`
-	OwnerName        string `json:"ownerName"`
-	Address1         string `json:"address1"`
-	Address2         string `json:"address2"`
-	State            string `json:"state"`
-	CoverImgURL      string `json:"coverimg"`
-	City             string `json:"city"`
-	Description      string `json:"description"`
-	Ammenities       string `json:"ammenities"`
-	Type             string `json:"type"`
-	VideoLink        string `json:"videoLink"`
-	RoomPhoto        string `json:"roomPhoto"`
-	Bathroom         string `json:"bathroomPhoto"`
-	Reception        string `json:"ReceptionPhoto"`
-	Kitchen          string `json:"kitchen"`
-	GmapLink         string `json:"gmap"`
-	NumberOfRooms    int    `json:"numberofrooms"`
-	OccupancyPerRoom int    `json:"occupancyperroom"`
-	Price            int    `json:"price"`
+	Id               primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	HostelName       string             `json:"hostelName"`
+	OwnerName        string             `json:"ownerName"`
+	Address1         string             `json:"address1"`
+	Address2         string             `json:"address2"`
+	State            string             `json:"state"`
+	CoverImgURL      string             `json:"coverimg"`
+	City             string             `json:"city"`
+	Description      string             `json:"description"`
+	Ammenities       string             `json:"ammenities"`
+	Type             string             `json:"type"`
+	VideoLink        string             `json:"videoLink"`
+	RoomPhoto        string             `json:"roomPhoto"`
+	Bathroom         string             `json:"bathroomPhoto"`
+	Reception        string             `json:"ReceptionPhoto"`
+	Kitchen          string             `json:"kitchen"`
+	GmapLink         string             `json:"gmap"`
+	NumberOfRooms    int                `json:"numberofrooms"`
+	OccupancyPerRoom int                `json:"occupancyperroom"`
+	Price            int                `json:"price"`
 	Users            []User
 }
