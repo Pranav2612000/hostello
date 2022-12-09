75118ffb3448b9fb312978e9d70abf177cd47a02
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-12-09 18:00:44 +0530

    chore: enable cors from anywhere

--- backend/hostello/server.go
+++ backend/hostello/server.go
@@ -7,6 +7,7 @@ import (
 	"hostello_app/hostello/service"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -49,6 +50,8 @@ func init() {
 	us = service.NewUser(userrc, ctx)
 	uc = controller.NewUserController(us)
 	server = gin.Default()
+    server.Use(cors.Default())
+
 }
 
 func main() {
