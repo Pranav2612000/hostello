7bcf74c1e66dea9e75f4d8d2e426d6cd702270a1
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-12-09 19:45:22 +0530

    improv: update assets

--- www/app/components/ZeroSlider/ZeroSlider.tsx
+++ www/app/components/ZeroSlider/ZeroSlider.tsx
@@ -11,13 +11,13 @@ import Wrapper from "../UI/Wrapper/Wrapper";
 import { Button, Center, Heading, Stack, Text } from "@chakra-ui/react";
 
 // Images
+import hero1 from "../../assets/images/hero1.png";
+import hero2 from "../../assets/images/hero2.png";
+import hero3 from "../../assets/images/hero3.jpg";
 //const hallstatt = "https://i.imgur.com/Yszno5e.jpg";
-const hallstatt = "https://images.unsplash.com/photo-1667470928088-57bfc3f28cf8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1452&q=80";
-const poolTable = "https://images.unsplash.com/photo-1661242686756-de9bdeb69ad8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
-const windowView = "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80";
-const hvitserkur = "https://i.imgur.com/ZBzbir7.jpg";
-const jacksonville = "https://i.imgur.com/xpeJkkR.jpg";
-const moraineLake = "https://i.imgur.com/0NAc45h.jpg";
+//const hallstatt = "https://images.unsplash.com/photo-1667470928088-57bfc3f28cf8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1452&q=80";
+//const poolTable = "https://images.unsplash.com/photo-1661242686756-de9bdeb69ad8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+//const windowView = "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80";
 
 const app = () => {
   return (
@@ -59,7 +59,7 @@ const app = () => {
     <Slide
       label="Giau Pass - Italy"
       background={{
-        backgroundImageSrc: hallstatt,
+        backgroundImageSrc: hero1,
         backgroundAnimation: "zoom"
       }}
     />
@@ -67,7 +67,7 @@ const app = () => {
     <Slide
       label="Bogliasco - Italy"
       background={{
-        backgroundImageSrc: poolTable,
+        backgroundImageSrc: hero2,
         backgroundAnimation: "zoom"
       }}
     />
@@ -75,7 +75,7 @@ const app = () => {
     <Slide
       label="County Clare - Ireland"
       background={{
-        backgroundImageSrc: windowView 
+        backgroundImageSrc: hero3 
       }}
     />
 
