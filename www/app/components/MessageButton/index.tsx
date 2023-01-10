3254ee4a96c36817703221c3d003c948d029d1f6
rishim1402 <rishim1402@gmail.com>
2023-01-10 14:07:06 +0530

    Add whatsapp number

--- www/app/components/MessageButton/index.tsx
+++ www/app/components/MessageButton/index.tsx
@@ -1,7 +1,7 @@
 import whatsappIcon from "../../assets/icons/whatsApp.png";
 const MessageButton = () => {
   return (
-    <a href="#" className="float">
+    <a href="wa.me/+919309747178" className="float">
       <img src={whatsappIcon} className="my-float" height={'40px'} width={'40px'}/>
     </a>       
   )
